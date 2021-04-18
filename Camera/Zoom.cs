1ff54f99258a4fb3d70db17e35080a1099f90dc7
Andres <andrespablo994@gmail.com>
2021-04-18 17:28:12 -0300

    Create Zoom.cs

---
+++ Camera/Zoom.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    public float zoomMin = 4;
+    public float zoomMax = 8;
+    public float zoomSpeed = 2;
+
+
+    void Update()
+    {
+        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        if(Camera.main.orthographicSize > zoomMax)
+            Camera.main.orthographicSize = zoomMax;
+        else if(Camera.main.orthographicSize < zoomMin)
+            Camera.main.orthographicSize = zoomMin;
+    }
+}
