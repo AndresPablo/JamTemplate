b1ef3fa51d1beb4529c6c9e2da571e6c9d03b655
Andres <andrespablo994@gmail.com>
2021-04-18 17:33:30 -0300

    Create LevelLoader.cs

---
+++ Scene Scripts/LevelLoader.cs
@@ -0,0 +1,15 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
