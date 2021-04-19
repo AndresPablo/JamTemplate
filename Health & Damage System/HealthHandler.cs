58d1c6a9f820fa1b0f0e611e296999cbc96d7007
Andres <andrespablo994@gmail.com>
2021-04-19 13:16:23 -0300

    Create HealthHandler.cs

---
+++ Health & Damage System/HealthHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine.Events;
+using UnityEngine;
+
+public class HealthHandler : MonoBehaviour, IDamageable
+{
+    [Min(1)]public float maxHealth = 100;
+    [SerializeField]float health = 100;
+    [SerializeField]GameObject deathPrefab;
+    
+    protected virtual void Start() {
+        health = maxHealth;
+    }
+    
+    public virtual void TakeDamage(DamageData _data)
+    {
+        health -= _data.amount;
+        if(health <= 0)
+        {
+            if(deathPrefab) Instantiate(deathPrefab, transform.position, transform.rotation);
+            Kill();
+        }
+    }
+    
+    public virtual void  Kill()
+    {
+        DestroSelf();
+    }
+    
+    public virtual void DestroSelf()
+    {
+        Destroy(gameObject);
+    }
+}
