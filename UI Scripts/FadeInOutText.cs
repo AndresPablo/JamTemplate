efc83a4effca0cd5c31b897c51577df476dcd7a1
Andres <andrespablo994@gmail.com>
2021-04-19 11:21:30 -0300

    Create FadeInOutText.cs

---
+++ UI Scripts/FadeInOutText.cs
@@ -0,0 +1,37 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FadeText : MonoBehaviour
+{
+    [SerializeField] float defaultTime = 2;
+    Color initialColor;
+    Text text;
+
+    
+    void Start()
+    {
+        text = GetComponent<Text>();
+        initialColor = text.color;
+    }
+
+
+    public void FadeIn(float _time = 1)
+    {
+        CancelInvoke();
+        Invoke("ToogleTextComponent", _time);
+        text.CrossFadeAlpha(255f, _time, false);
+    }
+
+    public void FadeOut(float _time = 1)
+    {
+        CancelInvoke();
+        Invoke("ToogleTextComponent", _time);
+        text.CrossFadeAlpha(0f, _time, false);
+    }
+
+    public void ToogleTextComponent()
+    {
+        text.enabled = !text.isActiveAndEnabled;
+    }
+
+}
