c0a48c5a567de5f3de2bb28565a78442a41bd916
Andres <andrespablo994@gmail.com>
2021-04-19 12:02:07 -0300

    Create FloatingText.cs

---
+++ UI Scripts/FloatingText.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0,2,0);
+
+    void Start()
+    {
+        transform.localPosition += offset;
+    }
+
+}
