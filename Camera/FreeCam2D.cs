fd7498b36a458df9dc2b953d3e94f549359bd14d
Andres <andrespablo994@gmail.com>
2021-11-29 12:17:09 -0300

    Update FreeCam2D.cs

--- Camera/FreeCam2D.cs
+++ Camera/FreeCam2D.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class FreeCam2D : MonoBehaviour
 {
-    public float speed = .08f;
+    public float speed = 1f;
+    public Vector2 boundaryBox = new Vector2(32,18);
     Vector2 dir;
 
 
@@ -14,26 +15,39 @@ public class FreeCam2D : MonoBehaviour
     
     void Update()
     {
+        Mover();
+    }
+
+    void Mover()
+    {
         dir.x = 0;
         dir.y = 0;
 
-        if(Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.W) && transform.position.y < boundaryBox.y)
         {
             dir.y = 1;
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.S) && transform.position.y > -boundaryBox.y)
         {
             dir.y = -1;
         }
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.A) && transform.position.x > -boundaryBox.x)
         {
             dir.x = -1;
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D) && transform.position.x < boundaryBox.x)
         {
             dir.x = 1;
         }
 
         transform.position += (Vector3)dir * speed;
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Vector3.zero, 
+            new Vector3(boundaryBox.x, boundaryBox.y, 0));
+    }
+
 }
